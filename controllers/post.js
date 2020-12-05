ecbf1e2c3906ee6e1c315f36af78e1059bdeec19
HunterWilkins <wilkins.hunter@gmail.com>
2020-12-04 23:27:17 -0600

    Added basic post 'post' route (probably won't need this for the final product, as users won't be able to post posts)

--- controllers/post.js
+++ controllers/post.js
@@ -3,8 +3,13 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/all", (req, res) => {
-    db.Post.findAll({}).then(dbPost => res.json(dbPosts))
+    db.Post.findAll({}).then(dbPosts => res.json(dbPosts))
     .catch(err => res.json(err));
 });
+
+router.post("/", (req, res) => {
+    db.Post.create(req.body).then(dbPost => res.json(dbPost))
+    .catch(err => res.json(err));
+})
 
 module.exports = router;

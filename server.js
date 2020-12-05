be403e2ee0b961e90254bc5ce6b8c98efa28ba0f
HunterWilkins <wilkins.hunter@gmail.com>
2020-12-04 21:40:17 -0600

    Added initial route and 'express.static('client/build')'

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
+const path = require("path");
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+};
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log("The Good Stuff is Listening on Port " + PORT);
